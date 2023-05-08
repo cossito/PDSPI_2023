83a2ce5fdc15397ec78cd379c4a48ebae270d9b7
cossito <h.cossito@escolar.ifrn.edu.br>
2023-05-08 14:25:59 -0300

    Terminei a estilização do CRUD com jQuery e Bootstrap.

--- WebAppProjeto2023/WebAppProjeto2023/Controllers/FabricantesController.cs
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/FabricantesController.cs
@@ -104,6 +104,7 @@ namespace WebAppProjeto2023.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante \"" + fabricante.Nome + "\" foi removido";
             return RedirectToAction("Index");
         }
     }
