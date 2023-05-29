5a9b46eeada367d6e1583fc3cf1f742b8b22f069
cossito <h.cossito@escolar.ifrn.edu.br>
2023-05-29 14:08:49 -0300

    Adicionei o Create do Produtos.

--- WebAppProjeto2023/WebAppProjeto2023/Controllers/ProdutosController.cs
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/ProdutosController.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebAppProjeto2023.Models;
+using System.Data.Entity;
 
 namespace WebAppProjeto2023.Controllers
 {
     public class ProdutosController : Controller
     {
         private EFContext context = new EFContext();
+
         // GET: Produtos
         public ActionResult Index()
         {
-            return View(context.Produtos.OrderBy(c => c.Nome));
+            var produtos = context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).OrderBy(n => n.Nome);
+            return View(produtos);
         }
 
         // GET: Produtos/Details/5
@@ -25,22 +28,25 @@ namespace WebAppProjeto2023.Controllers
         // GET: Produtos/Create
         public ActionResult Create()
         {
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome), "CategoriaId", "Nome");
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome), "FabricanteId", "Nome");
             return View();
         }
 
         // POST: Produtos/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Produto produto)
         {
             try
             {
                 // TODO: Add insert logic here
-
+                context.Produtos.Add(produto);
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(produto);
             }
         }
 
